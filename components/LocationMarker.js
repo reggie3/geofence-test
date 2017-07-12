5172cab1cacf297a922d203ed99cd3863c26d6cc
Reginald Johnson <reginald.johnson@gamil.com>
2017-07-11 19:32:34 -1000

    does not work

--- components/LocationMarker.js
+++ components/LocationMarker.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import { View, Animated, StyleSheet } from 'react-native';
 import Expo from 'expo';
 import { connect } from 'react-redux';
-
+import AnimatedSprite from 'react-native-animated-sprite';
+import monsterSprite from '../assets/sprites/monster/monsterSprite';
 
 class LocationMarker extends Component {
     constructor(props) {
         super(props);
         this.state = {
             fadeAnim: new Animated.Value(0),  // Initial value for opacity: 0
-            bounceDuration: 1
+            bounceDuration: 1,
+             animationType: 'WALK',
         };
     }
 
@@ -36,14 +38,28 @@ class LocationMarker extends Component {
     render() {
         return (
 
-                <Expo.MapView.Marker
-                    key={this.props.key}
-                    image={require('../assets/images/usa-american-flag-waving-animated-gif-26.gif')}
-                    coordinate={{
-                        latitude: this.props.location.loc[0],
-                        longitude: this.props.location.loc[1],
+            <Expo.MapView.Marker
+                key={this.props.key}
+                coordinate={{
+                    latitude: this.props.location.loc[0],
+                    longitude: this.props.location.loc[1],
+                }}>
+                <AnimatedSprite
+                    ref={'monsterRef'}
+                    sprite={monsterSprite}
+                    animationFrameIndex={monsterSprite.animationIndex(this.state.animationType)}
+                    loopAnimation={true}
+                    coordinates={{
+                        top: 100,
+                        left: 100,
                     }}
+                    size={{
+                        width: monsterSprite.size.width * 1.65,
+                        height: monsterSprite.size.height * 1.65,
+                    }}
+                    onPress={() => { this.onPress(); }}
                 />
+            </Expo.MapView.Marker>
 
         )
     }
